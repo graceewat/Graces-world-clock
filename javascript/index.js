1832ebffe964071ba9670f1d202f9c524f92463b
graceewat <160866251+graceewat@users.noreply.github.com>
2024-06-11 10:48:03 -0400

    Added moment.js and moment-timezone to update to real time and dates. Added setInterval to update every second.

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
+// London
+function updateTime() {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonCurrentTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonCurrentTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = `${londonCurrentTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  )}`;
 
+  // New York
+
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkCurrentTime = moment().tz("America/New-York");
+  newYorkDateElement.innerHTML = newYorkCurrentTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = `${newYorkCurrentTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  )}`;
+}
+updateTime();
+setInterval(updateTime, 1000);
