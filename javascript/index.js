b6cd706cafa02f0ce903f75b2d86207cffd6fc50
graceewat <160866251+graceewat@users.noreply.github.com>
2024-06-13 08:55:24 -0400

    Add link to all cities and css styling

--- javascript/index.js
+++ javascript/index.js
@@ -42,7 +42,8 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss[<small>]A[</small>]"
           )}</div>
-        </div>`;
+        </div>
+  <a href="/">All Cities</a>`;
 }
 
 updateTime();
